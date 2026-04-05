f8742f4c212c367fe05ceba62fec98de16644a89
Nolan <nolan.cotter@icloud.com>
2026-04-05 18:43:37 -0400

    Refine Svelte 5 input component typing

--- src/lib/components/ui/input.svelte
+++ src/lib/components/ui/input.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  let { value = $bindable(''), className = '', ...rest } = $props<{
+  type InputProps = {
     value?: string | number | undefined;
     className?: string;
-  }>();
+    [key: string]: unknown;
+  };
+
+  let { value = $bindable(''), className = '', ...rest }: InputProps = $props();
 </script>
 
 <input bind:value={value} class={`input ${className}`.trim()} {...rest} />
